145c6ae2683fd65ff9a2ce8f7d91ef30df0d7ceb
avernekar <anup.vernekar@quicken.com>
2024-10-21 00:15:02 +0530

    changes to resolve bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,10 @@ import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
-    <motion.div
+    {/* <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-    >
+    > */}
       <div
         options={{
           max: 45,
@@ -27,19 +27,19 @@ const ServiceCard = ({ index, title, icon }) => (
           {title}
         </h3>
       </div>
-    </motion.div>
+    {/* </motion.div> */}
   </Tilt>
 );
 
 const About = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      {/* <motion.div variants={textVariant()}> */}
         <p className={`${styles.sectionSubText} text-center`}>Introduction</p>
         <h2 className={`${styles.sectionHeadText} text-center`}>Overview</h2>
-      </motion.div>
+      {/* </motion.div> */}
 
-      <motion.p
+      <p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] text-center leading-[30px]"
       >
@@ -51,7 +51,7 @@ const About = () => {
         interfaces. I’m passionate about solving complex technical challenges,
         collaborating in fast-paced environments, and learning new tools and
         technologies.
-      </motion.p>
+      </p>
 
       <div className="mt-20 flex flex-wrap gap-10 justify-center">
         {services.map((service, index) => (
