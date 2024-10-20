e7869e2c98f76fdfe2086f2c18c94e3d9c36e977
avernekar <anup.vernekar@quicken.com>
2024-10-21 00:48:26 +0530

    removed some code

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <StarsCanvas />
+          {/* <StarsCanvas /> */}
           <Hero />
         </div>
         <About />
